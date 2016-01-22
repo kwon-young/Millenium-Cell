755f3f0b3ed248ff75b9933786e196247221f541
kwon-young <kwon-young.choi@hotmail.fr>
2016-01-22 13:21:42 +0100

    Added comments

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -8,269 +8,16 @@
 #include <cstdlib>
 #include <algorithm>
 
-/*
-
-Form::Form (boost::dynamic_bitset<> &form, std::vector<unsigned int> &dim):
-  _form(form),
-  _dim(dim),
-  _pointsStructuredGrid(vtkSmartPointer<vtkStructuredGrid>::New()),
-  _points(vtkSmartPointer<vtkPoints>::New()),
-  _pointsGeometryFilter(vtkSmartPointer<vtkStructuredGridGeometryFilter>::New()),
-  _pointsMapper(vtkSmartPointer<vtkPolyDataMapper>::New()),
-  _pointsActor(vtkSmartPointer<vtkActor>::New()),
-  _inputs(new vtkPolyData*[form.size()]),
-  _cubesSource(new vtkCubeSource*[form.count()]),
-  _cubesAppendFilter(vtkSmartPointer<vtkAppendPolyData>::New()),
-  _cubesCleanFilter(vtkSmartPointer<vtkCleanPolyData>::New()),
-  _cubesMapper(vtkSmartPointer<vtkPolyDataMapper>::New()),
-  _cubesActor(vtkSmartPointer<vtkActor>::New())
-{
-  _cubesCleanFilter->SetInputConnection(_cubesAppendFilter->GetOutputPort());
-  //Create a mapper and actor
-  _cubesMapper->SetInputConnection(_cubesCleanFilter->GetOutputPort());
-  _cubesActor->SetMapper(_cubesMapper);
-
-#if VTK_MAJOR_VERSION <= 5
-  _pointsGeometryFilter->SetInputConnection(_pointsStructuredGrid->GetProducerPort());
-#else
-  _pointsGeometryFilter->SetInputData(_pointsStructuredGrid);
-#endif
-  // Create a mapper and actor
-  _pointsMapper->SetInputConnection(_pointsGeometryFilter->GetOutputPort());
-
-  _pointsActor->SetMapper(_pointsMapper);
-  _pointsActor->GetProperty()->SetPointSize(1);
-  convertPointGrid();
-  convertRectangleList();
-}
-
-Form::~Form ()
-{
-  if (_inputs)
-    delete[] _inputs;
-  if (_cubesSource)
-    delete[] _cubesSource;
-}
-
-void Form::convertPointGrid()
-{
-  if (_dim.size() == 2) {
-    for(unsigned int k = 0; k < _dim[2]; k++)
-    {
-      for(unsigned int j = 0; j < _dim[1]; j++)
-      {
-        _points->InsertNextPoint(j, k, 1);
-      }
-    }
-  } else if (_dim.size() == 3) {
-    for(unsigned int k = 0; k < _dim[2]; k++)
-    {
-      for(unsigned int j = 0; j < _dim[1]; j++)
-      {
-        for(unsigned int i = 0; i < _dim[0]; i++)
-        {
-          _points->InsertNextPoint(i, j, k);
-        }
-      }
-    }
-  }
-
-  // Specify the dimensions of the grid
-  if(_dim.size()==2)
-    _pointsStructuredGrid->SetDimensions(_dim[0], _dim[1], 1);
-  else if(_dim.size() == 3)
-    _pointsStructuredGrid->SetDimensions(_dim[0], _dim[1], _dim[2]);
-  _pointsStructuredGrid->SetPoints(_points);
-
-  _pointsGeometryFilter->Update();
-
-}
-
-void Form::getXYZ(
-    const unsigned int pos,
-    unsigned int &x,
-    unsigned int &y,
-    unsigned int &z)
-{
-  x = (pos % (_dim[1]*_dim[0])) % _dim[1];
-  y = (pos % (_dim[1]*_dim[0])) / _dim[1];
-  z = pos / (_dim[1]*_dim[0]);
-}
-
-void Form::convertRectangleList()
-{
-  //std::cout << _form.size() << std::endl;
-  //std::cout << std::accumulate(_dim.begin(), _dim.end(), 1, std::multiplies<int>()) << std::endl;
-  assert(_form.size()==std::accumulate(_dim.begin(), _dim.end(), 1, std::multiplies<int>()));
-  assert(_dim.size() == 2 || _dim.size() == 3);
-
-  if (_form.count() > 0)
-  {
-    int pos = _form.find_first();
-    unsigned int x, y, z;
-    for (unsigned int i = 0; i < _form.count(); ++i) {
-      if (pos != -1)
-      {
-        getXYZ(pos, x, y, z);
-        //std::cout << "pos : " << pos << std::endl;
-        //std::cout << x << std::endl;
-        //std::cout << y << std::endl;
-        //std::cout << z << std::endl;
-        //std::cout << "end" << std::endl;
-        _cubesSource[i] = vtkCubeSource::New();
-        _cubesSource[i]->SetCenter(x, y, z);
-        _cubesSource[i]->SetXLength(0.8);
-        _cubesSource[i]->SetYLength(0.8);
-        _cubesSource[i]->SetZLength(0.8);
-        _cubesSource[i]->Update();
-        _inputs[i] = vtkPolyData::New();
-        _inputs[i]->ShallowCopy(_cubesSource[i]->GetOutput());
-#if VTK_MAJOR_VERSION <= 5
-        _cubesAppendFilter->AddInputConnection(_inputs[i]->GetProducerPort());
-#else
-        _cubesAppendFilter->AddInputData(_inputs[i]);
-#endif
-      }
-      pos = _form.find_next(pos);
-    }
-    _cubesAppendFilter->Update();
-
-    // Remove any duplicate points.
-    _cubesCleanFilter->Update();
-  }
-}
-
-vtkSmartPointer<vtkActor> Form::getPointsActor() const
-{
-  return _pointsActor;
-}
-
-vtkSmartPointer<vtkActor> Form::getCubesActor() const
-{
-  return _cubesActor;
-}
-
-void KeypressCallbackFunction (
-  vtkObject* caller,
-  long unsigned int vtkNotUsed(eventId),
-  void* clientData,
-  void* vtkNotUsed(callData) )
-{
-
-  vtkRenderWindowInteractor *iren = 
-    static_cast<vtkRenderWindowInteractor*>(caller);
- 
-  std::cout << "Pressed: " << iren->GetKeySym() << std::endl;
-
-  Env *env = static_cast<Env*>(clientData);
-  env->updateForms(iren->GetKeySym());
-
-  env->Render();
-  //// Close the window
-  //iren->GetRenderWindow()->Finalize();
- 
-  //// Stop the interactor
-  //iren->TerminateApp();
-  //std::cout << "Closing window..." << std::endl;
-  
-}
-
-Env::Env (
-      std::vector<double> bgColor,
-      std::vector<unsigned int> dim,
-      Graph &g,
-      std::vector<unsigned int> verticesPerTimestep) :
-  _bgColor(bgColor),
-  _dim(dim),
-  _g(g),
-  _verticesPerTimestep(verticesPerTimestep),
-  _vertexPair(vertices(_g)),
-  _forms(),
-  _renderIndex(0),
-  _renderer(vtkSmartPointer<vtkRenderer>::New()),
-  _renderWindow(vtkSmartPointer<vtkRenderWindow>::New()),
-  _renderWindowInteractor(vtkSmartPointer<vtkRenderWindowInteractor>::New()),
-  _keypressCallback(vtkSmartPointer<vtkCallbackCommand>::New())
-{
-  _renderWindow->AddRenderer(_renderer);
-  _renderWindowInteractor->SetRenderWindow(_renderWindow);
-  _renderer->SetBackground(_bgColor[0], _bgColor[1], _bgColor[2]);
-
-  _keypressCallback->SetCallback ( KeypressCallbackFunction );
-  _keypressCallback->SetClientData(this);
-  _renderWindowInteractor->AddObserver (
-      vtkCommand::KeyPressEvent,
-      _keypressCallback );
-  
-  int verticesNbr = std::accumulate(_verticesPerTimestep.begin(), _verticesPerTimestep.end(), 0);
-  _forms.resize(verticesNbr, NULL);
-
-  updateForms("");
-  _renderer->AddActor(_forms[_renderIndex]->getCubesActor());
-  _renderer->AddActor(_forms[_renderIndex]->getPointsActor());
-}
-
-Env::~Env ()
-{
-  for (int i = 0; i < _forms.size(); ++i) {
-    if(_forms[i] != NULL) delete _forms[i];
-  }
-}
-
-//void Env::addForm(Form *myForm)
-//{
-  //_forms.push_back(myForm);
-  //_renderer->AddActor(myForm->getCubesActor());
-  //_renderer->AddActor(myForm->getPointsActor());
-//}
-
-void Env::getFormFromGraph()
-{
-  _vertexPair = vertices(_g);
-  for (int i = 0; i < _renderIndex; ++i) {
-    ++_vertexPair.first;
-  }
-  boost::dynamic_bitset<> gform = _g[*_vertexPair.first];
-  int maxCell = std::accumulate(_dim.begin(), _dim.end(), 1, std::multiplies<int>());
-  if (gform.size() == (maxCell / 2)) gform.resize(gform.size()*2, 0);
-  std::cout << gform.size() << std::endl;
-  _forms[_renderIndex] = new Form(gform, _dim);
-}
-
-void Env::updateForms(std::string keySym)
-{
-  int inc = 0;
-  if (!(keySym.compare("Right")))
-    inc = 1;
-  else if (!(keySym.compare("Left")))
-    inc = -1;
-
-  if (_renderIndex < 0 && _renderIndex >= _forms.size())
-  {
-    _renderIndex = 0;
-  }
-  if (_renderIndex+inc >= 0 && _renderIndex+inc < _forms.size())
-  {
-    _renderIndex+=inc;
-    std::cout << "update : " << _renderIndex << std::endl;
-  }
-  _renderer->Clear();
-  if(_forms[_renderIndex] == NULL) getFormFromGraph();
-
-}
-
-void Env::Render()
-{
-  _renderWindow->Render();
-}
-
-void Env::Start()
-{
-  _renderWindowInteractor->Start();
-}
-
-*/
-
+/* -----------------------------------------------------------*/
+/** 
+ * @brief Callback used to react to keypress in the vtk windows
+ * 
+ * @param caller
+ * @param eventId
+ * @param clientData
+ * @param callData
+ */
+/* -----------------------------------------------------------*/
 void KeypressCallbackFunction (
   vtkObject* caller,
   long unsigned int vtkNotUsed(eventId),
@@ -278,12 +25,17 @@ void KeypressCallbackFunction (
   void* vtkNotUsed(callData) )
 {
  
+  // Get the current window interactor
   vtkRenderWindowInteractor *iren = 
     static_cast<vtkRenderWindowInteractor*>(caller);
  
+  // Get the keysym pressed
   std::string key = iren->GetKeySym();
   //std::cout << "Pressed: " << key << std::endl;
 
+  /**
+   * For now, you can press right or left to iterate through all forms in the graph
+   */
   GraphViewer *gv = static_cast<GraphViewer*>(clientData);
   if (!key.compare("Left"))
     gv->setFormIndex(gv->getFormIndex()-1);
@@ -330,10 +82,12 @@ GraphViewer::GraphViewer (
   _form(),
   _vertexPair(vertices(_g))
 {
+  // Set the object flow for the vtk window
   _renderWindow->AddRenderer(_renderer);
   _renderWindowInteractor->SetRenderWindow(_renderWindow);
   _renderer->SetBackground(_bgColor[0], _bgColor[1], _bgColor[2]);
 
+  // Setup the keypress interactor
   _keypressCallback->SetCallback ( KeypressCallbackFunction );
   _keypressCallback->SetClientData(this);
   _renderWindowInteractor->AddObserver (
@@ -362,32 +116,20 @@ void GraphViewer::Start()
 
 void GraphViewer::convertPointGrid()
 {
-  if (_dim.size() == 2) {
-    for(int k = 0; k < _dim[2]; k++)
+  // Iterate through all dimension to set point
+  for(int k = 0; k < _dim[2]; k++)
+  {
+    for(int j = 0; j < _dim[1]; j++)
     {
-      for(int j = 0; j < _dim[1]; j++)
+      for(int i = 0; i < _dim[0]; i++)
       {
-        _points->InsertNextPoint(j, k, 1);
-      }
-    }
-  } else if (_dim.size() == 3) {
-    for(int k = 0; k < _dim[2]; k++)
-    {
-      for(int j = 0; j < _dim[1]; j++)
-      {
-        for(int i = 0; i < _dim[0]; i++)
-        {
-          _points->InsertNextPoint(i, j, k);
-        }
+        _points->InsertNextPoint(i, j, k);
       }
     }
   }
 
   // Specify the dimensions of the grid
-  if(_dim.size()==2)
-    _pointsStructuredGrid->SetDimensions(_dim[0], _dim[1], 2);
-  else if(_dim.size() == 3)
-    _pointsStructuredGrid->SetDimensions(_dim[0], _dim[1], _dim[2]);
+  _pointsStructuredGrid->SetDimensions(_dim[0], _dim[1], _dim[2]);
   _pointsStructuredGrid->SetPoints(_points);
 
 #if VTK_MAJOR_VERSION <= 5
@@ -420,16 +162,23 @@ int GraphViewer::getFormIndex() const
 
 void GraphViewer::setFormIndex(int newFormIndex)
 {
+  // Count the total number of form in the graph
   int maxForm = std::accumulate(
       _verticesPerTimestep.begin(),
       _verticesPerTimestep.end(),
       0);
+  // Count the difference from the current form index and the new form index
   int inc = std::min(std::max(0, newFormIndex), maxForm-1) - _formIndex;
+  // Get the new form
   _vertexPair.first += inc;
   _form = _g[*_vertexPair.first];
+  // Count the maximum number of cell that the space can contains
   int maxCell = std::accumulate(_dim.begin(), _dim.end(), 1, std::multiplies<int>());
+  // Verify if the number of cell in the form is correct
   if (_form.size() != maxCell) _form.resize(maxCell, 0);
+  // Set the new index
   _formIndex += inc;
+
   drawForm();
 }
 
@@ -437,6 +186,7 @@ void GraphViewer::resizeCubes(int size)
 {
   if (size > _cubesSource.size())
   {
+    // Add cubes
     int diff = size-_cubesSource.size();
     for (int i = 0; i < diff; ++i) {
       _cubesSource.push_back(vtkCubeSource::New());
@@ -449,6 +199,7 @@ void GraphViewer::resizeCubes(int size)
     }
   } else if (size < _cubesSource.size())
   {
+    // Remove cubes
     int diff = (size-_cubesSource.size());
     diff = std::abs(diff);
     for (int i = 0; i < diff; ++i) {
@@ -475,10 +226,12 @@ void GraphViewer::getXYZ(
 
 void GraphViewer::drawForm()
 {
+  // Check the number of cubes, resize if different
   if (_form.count() != _cubesSource.size())
   {
     resizeCubes(_form.count());
   }
+  // pos is used for the index of the current cube
   int pos = _form.find_first();
   int x, y, z;
   for (unsigned int i = 0; i < _cubesSource.size(); ++i) {
--- src/Graphics.hpp
+++ src/Graphics.hpp
@@ -1,4 +1,13 @@
+/** 
+ * @file Graphics.hpp
+ * @author Kwon-Young Choi
+ * @version 0.1
+ * @date 2016-01-21
+ */
 
+/**
+ * VTK includes
+ */
 #include <vtkVersion.h>
 #include <vtkSmartPointer.h>
 #include <vtkStructuredGrid.h>
@@ -20,137 +29,152 @@
 #include <vtkConeSource.h>
 #include <vtkPoints.h>
 
+/**
+ * Boost include
+ */
 #include <boost/dynamic_bitset.hpp>
 
+/**
+ * Standard library include
+ */
 #include <vector>
 #include <string>
 
+/**
+ * Project include
+ */
 #include "environment.h"
 
-/*
-
-class Form
-{
-public:
-  Form (boost::dynamic_bitset<> &form, std::vector<unsigned int> &dim);
-  virtual ~Form ();
-
-  void setForm(vtkSmartPointer<vtkAppendPolyData> cubeAppendFilter);
-private:
-
-  void getXYZ(
-      const unsigned int pos,
-      unsigned int &x,
-      unsigned int &y,
-      unsigned int &z);
-
-  boost::dynamic_bitset<> _form;
-  std::vector<unsigned int> _dim;
-
-  vtkPolyData **_inputs;
-  vtkCubeSource **_cubesSource;
-};
-
-void KeypressCallbackFunction (
-  vtkObject* caller,
-  long unsigned int eventId,
-  void* clientData,
-  void* callData );
-
-class Env
-{
-public:
-  Env (
-      std::vector<double> bgColor,
-      std::vector<unsigned int> dim,
-      Graph &g,
-      std::vector<unsigned int> verticesPerTimestep);
-  virtual ~Env ();
-
-  //void addForm(Form *myForm);
-
-  void getFormFromGraph();
-  void updateForms(std::string keySym);
-
-  void Render();
-  void Start();
-
-private:
-
-  void convertPointGrid();
-
-  std::vector<double> _bgColor;
-  std::vector<unsigned int> _dim;
-  Graph &_g;
-  std::vector<unsigned int> _verticesPerTimestep;
-  pair< vertex_iter, vertex_iter > _vertexPair;
-  std::vector<Form*> _forms;
-  unsigned int _renderIndex;
-
-  vtkSmartPointer<vtkStructuredGrid> _pointsStructuredGrid;
-  vtkSmartPointer<vtkPoints> _points;
-  vtkSmartPointer<vtkStructuredGridGeometryFilter> _pointsGeometryFilter;
-  vtkSmartPointer<vtkPolyDataMapper> _pointsMapper;
-  vtkSmartPointer<vtkActor> _pointsActor;
-
-  vtkSmartPointer<vtkAppendPolyData> _cubesAppendFilter;
-  vtkSmartPointer<vtkCleanPolyData> _cubesCleanFilter;
-  vtkSmartPointer<vtkPolyDataMapper> _cubesMapper;
-  vtkSmartPointer<vtkActor> _cubesActor;
-
-  vtkSmartPointer<vtkRenderer> _renderer;
-  vtkSmartPointer<vtkRenderWindow> _renderWindow;
-  vtkSmartPointer<vtkRenderWindowInteractor> _renderWindowInteractor;
-  vtkSmartPointer<vtkCallbackCommand> _keypressCallback;
-};
-
-*/
-
+/* -----------------------------------------------------------*/
+/** 
+ * @brief Class allowing to view a Graph object with VTK
+ */
+/* -----------------------------------------------------------*/
 class GraphViewer
 {
 public:
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Constructor
+   * 
+   * @param g : Graph object to view, contains Boost::dynamic_bitset<> of forms
+   * @param verticesPerTimestep : number of forms for each level of the graph
+   * @param bgColor : background color for the vtk windows
+   * @param dim : 3D space size
+   */
+  /* -----------------------------------------------------------*/
   GraphViewer (
       Graph &g,
       std::vector<int> verticesPerTimestep,
       std::vector<double> bgColor,
       std::vector<int> dim);
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Destructor
+   */
+  /* -----------------------------------------------------------*/
   virtual ~GraphViewer ();
 
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Getter for _formIndex
+   * 
+   * @return _formIndex
+   */
+  /* -----------------------------------------------------------*/
   int getFormIndex() const;
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Setter for _formIndex
+   * 
+   * @param newFormIndex : new index for _formIndex
+   */
+  /* -----------------------------------------------------------*/
   void setFormIndex(int newFormIndex);
 
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Perform render action for vt
+   */
+  /* -----------------------------------------------------------*/
   void Render();
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Start the windows for vtk
+   */
+  /* -----------------------------------------------------------*/
   void Start();
 private:
 
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Create 3D grid of VTK point
+   */
+  /* -----------------------------------------------------------*/
   void convertPointGrid();
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Set connections for the VTK object flow
+   */
+  /* -----------------------------------------------------------*/
   void initCubes();
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Convert a 1D coordinate to 3D coordinates
+   * 
+   * @param[in] pos : 1D coordinate that represent a 3D coordinate
+   * @param[out] x : x parameter of the 3D coordinate
+   * @param[out] y : y parameter of the 3D coordinate
+   * @param[out] z : z parameter of the 3D coordinate
+   */
+  /* -----------------------------------------------------------*/
   void getXYZ(
       const int pos,
       int &x,
       int &y,
       int &z);
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Resize the number of cubes to be drawn by vtk
+   * 
+   * @param[in] size : new number of cubes to be drawn
+   */
+  /* -----------------------------------------------------------*/
   void resizeCubes(int size);
+  /* -----------------------------------------------------------*/
+  /** 
+   * @brief Convert a form into cubes to be drawn by vtk
+   */
+  /* -----------------------------------------------------------*/
   void drawForm();
-  void updateCubes();
 
   /* data */
-  Graph &_g;
-  std::vector<int> _verticesPerTimestep;
-  std::vector<double> _bgColor;
-  std::vector<int> _dim;
+  Graph &_g; /*!< graph of form to be drawn*/
+  std::vector<int> _verticesPerTimestep; /*!< number of forms for each level of the graph*/
+  std::vector<double> _bgColor; /*!< background color for the vtk window*/
+  std::vector<int> _dim; /*!< Dimension of the 3D space*/
 
+  /**
+   * VTK objects concerning the windows and its interactor
+   */
   vtkSmartPointer<vtkRenderer> _renderer;
   vtkSmartPointer<vtkRenderWindow> _renderWindow;
   vtkSmartPointer<vtkRenderWindowInteractor> _renderWindowInteractor;
   vtkSmartPointer<vtkCallbackCommand> _keypressCallback;
 
+  /**
+   * VTK objects for drawing a grid of points
+   */
   vtkSmartPointer<vtkStructuredGrid> _pointsStructuredGrid;
   vtkSmartPointer<vtkPoints> _points;
   vtkSmartPointer<vtkStructuredGridGeometryFilter> _pointsGeometryFilter;
   vtkSmartPointer<vtkPolyDataMapper> _pointsMapper;
   vtkSmartPointer<vtkActor> _pointsActor;
 
+  /**
+   * VTK objects for drawing multiple cubes
+   * Used for drawing a form in the vtk windows
+   */
   std::vector<vtkCubeSource*> _cubesSource;
   std::vector<vtkPolyData*> _inputs;
   vtkSmartPointer<vtkAppendPolyData> _cubesAppendFilter;
@@ -158,7 +182,7 @@ private:
   vtkSmartPointer<vtkPolyDataMapper> _cubesMapper;
   vtkSmartPointer<vtkActor> _cubesActor;
 
-  int _formIndex;
-  boost::dynamic_bitset<> _form;
-  pair< vertex_iter, vertex_iter > _vertexPair;
+  int _formIndex; /*!< index of the current form*/
+  boost::dynamic_bitset<> _form; /*!< dynamic_bytset of the current form*/
+  pair< vertex_iter, vertex_iter > _vertexPair; /*!< iterator for iterating the graph*/
 };
